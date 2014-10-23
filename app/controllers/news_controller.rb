98a7f817fe5a94e79e74644e0f0e8b472aa5b683
Chris <christopherdieckhaus@gmail.com>
2014-10-23 17:58:55 +0100

    Update news page with N&O article

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,10 @@
 class NewsController < ApplicationController
   def news
-    @articles = [{title: "Why We Need More Disruptive Women",
+    @articles = [{title: "Girls Engineering Change wants to close gender gap in engineering",
+                  site: "News & Observer",
+                  src: "http://www.newsobserver.com/2014/10/22/4254650/girls-engineering-change-wants.html",
+                  blurb: "Only 18 to 20 percent of engineers across the U.S. are women, and only 18 percent of those studying                   to be engineers in universities are women, according to a recent article published by American Society of                     Mechanical Engineers. Those statistics motivated Schindler, now a senior at Duke, to found Girls                             Engineering Change in summer 2012, a nonprofit organization dedicated to closing the gender gap in                           engineering..."},      
+            {title: "Why We Need More Disruptive Women",
                  site: "The Shriver Report",
                  src: "http://shriverreport.org/how-to-get-more-women-interested-in-stem/",
                  blurb: "Duke University's engineering labs are usually quiet on the weekends.
@@ -9,7 +13,7 @@ class NewsController < ApplicationController
                     engineering majors watched on, we all began to smile - our
                     physics-weary faces brightened by the sounds of tween girls learning
                     how to solder..."},
-                 {title: "Getting to the STEM of Gender Inequality",
+           {title: "Getting to the STEM of Gender Inequality",
                  site: "The Huffington Post",
                  src: "http://www.huffingtonpost.com/chelsea-clinton/getting-to-the-stem-of-ge_b_3002058.html",
                  blurb: "'...Many of the young female innovators and change-makers focus their
@@ -19,7 +23,7 @@ class NewsController < ApplicationController
                     who emerged from CGI U last year as an engineering evangelist.
                     Christine made a CGI U 2012 Commitment to Action pairing female
                     engineering students at Duke with teenage girls in Durham...' - Chelsea Clinton"},
-                 {title: "College Mentors Key to Prospective Female STEM Majors",
+           {title: "College Mentors Key to Prospective Female STEM Majors",
                  site: "U.S. News",
                  src: "http://www.usnews.com/education/high-schools/articles/2012/05/31/college-mentors-key-to-prospective-female-stem-majors",
                  blurb: "A mentor would have made Duke University student Christine Schindler
@@ -28,12 +32,12 @@ class NewsController < ApplicationController
                     she says are constantly exciting and clue her into new career
                     possibilities frequently, Schindler hopes to provide support to young
                     women that she once lacked..."},
-                 {title: "Girls Engineering Change - CGI U 2013 (video)",
+           {title: "Girls Engineering Change - CGI U 2013 (video)",
                  site: "Clinton Global Initiative",
                  src: "https://www.youtube.com/watch?v=gntsK6Rud2M",
                  blurb: "After participating in a summer engineering program at Vanderbilt, Christine Schindler of Duke University developed a deep passion for engineering. During the program, Christine realized that most people hold many
                     misconceptions about engineering..."},
-                 {title: "Christine Schindler: Bill Clinton knows her name, so you should too!",
+           {title: "Christine Schindler: Bill Clinton knows her name, so you should too!",
                  site: "HerCampus",
                  src: "http://www.hercampus.com/school/duke/christine-schindler-bill-clinton-knows-her-name-so-you-should-too-0",
                  blurb: "Considering former President Bill Clinton mentioned her name at his Global Initiative conference
